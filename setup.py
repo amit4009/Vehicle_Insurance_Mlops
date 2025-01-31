b33f9889c52edb6721ccd7dd6f56654c2bb75e01
Amit Pandit <i2pandit@gmail.com>
2025-01-31 04:12:18 -0500

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Amit Pandit",
+    author_email="i2pandit@gmail.com",
+    packages=find_packages()
+)
